7666f03a43534bd2f97b49db14df59da9c0dbc7a
Justin Garnica <60009814@ironschools.org>
2018-09-19 10:19:51 -0600

    added Ex_1_9

---
+++ Chapter 1/Ex1_9.java
@@ -0,0 +1,12 @@
+/*
+Author: Justin Garnica
+Date: 9/19/2018
+
+Output result of equation
+*/
+
+class Ex_1_9 {
+	public static void main(String[] args) {
+		System.out.println( (4.5 * 7.9));
+	}
+}
