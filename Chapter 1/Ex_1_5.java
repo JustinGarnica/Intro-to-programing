93defaf6e71359d2ce1b80cc781470d887f6a5a7
Justin Garnica <60009814@ironschools.org>
2018-09-19 10:10:27 -0600

    Created Ex_1_5.java

---
+++ Chapter 1/Ex_1_5.java
@@ -0,0 +1,12 @@
+/*
+Author: Justin Garnica
+Date: 9/19/2018
+
+Output result of equation
+*/
+
+class Ex_1_5 {
+	public static void main(String[] args) {
+		System.out.println( ( (9.5 * 4.5) - ( 2.5 * 3) ) / (45.5 - 3.5) );
+	}
+}
