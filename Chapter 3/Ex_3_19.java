3ad6bce64a9f44100c713ee4b358f78fcdff129b
Justin garnica <justin.garnica@suusuccess.org>
2018-10-19 08:17:47 -0600

    revised code to fit requirements

--- Chapter 3/Ex_3_19.java
+++ Chapter 3/Ex_3_19.java
@@ -12,32 +12,21 @@ public class Ex_3_19 {
 		Scanner input = new Scanner(System.in);
 
 		System.out.println("Enter three edges for a triangle:");
-		System.out.print(" Edge 1 point x: ");
-		double x1 = input.nextDouble();
-		System.out.print(" Edge 1 point y: ");
-		double y1 = input.nextDouble();
-		System.out.print(" Edge 2 point x: ");
-		double x2 = input.nextDouble();
-		System.out.print(" Edge 2 point y: ");
-		double y2 = input.nextDouble();
-		System.out.print(" Edge 3 point x: ");
-		double x3 = input.nextDouble();
-		System.out.print(" Edge 3 point y: ");		
-		double y3 = input.nextDouble();
-
-		boolean valid = (x1 + y1 > x3 + y3 && x2 + y2 > x3 + y3) ||
-							 (x1 + y1 > x2 + y2 && x3 + y3 > x2 + y2) ||
-							 (x3 + y3 > x1 + y1 && x2 + y2 > x1 + y1);
+		System.out.print(" side 1: ");
+		double side1 = input.nextDouble();
+		System.out.print(" side 2: ");
+		double side2 = input.nextDouble();
+		System.out.print(" side 3: ");
+		double side3 = input.nextDouble();
+				boolean valid = (side1 > side3 && side2 > side3) &&
+							 (side1 > side2 && side3 > side2) &&
+							 (side3 > side1 && side2 > side1);
 
 		if (!valid)
 		{
 			System.out.println("Input is invalid.");
 			System.exit(1);
 		}
-		
-		double side1 = Math.pow(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2), 0.5);
-		double side2 = Math.pow(Math.pow(x3 - x2, 2) + Math.pow(y3 - y2, 2), 0.5);
-		double side3 = Math.pow(Math.pow(x1 - x3, 2) + Math.pow(y1 - y3, 2), 0.5);
 
 		System.out.println("perimeter of triangle is " + (side1 + side2 + side3));
 	}
