af21496541e02303ce4b43acdb1c4b9e4b4b9197
Justin Garnica <justin.garnica@suusuccess.org>
2018-10-30 08:33:43 -0600

    Started payroll code

---
+++ Chapter 4/Ex_4_23.java
@@ -0,0 +1,15 @@
+		/*
+		Author: Justin Garnica
+		Date: 10/26/2018
+
+		Tells the payroll of any person
+		*/
+		import java.util.Scanner;
+
+		public class Ex_4_23{
+			public static void main(String[] args) {
+				Scanner input = new Scanner(System.in);
+			
+
+	}
+}
