d398513e6622a1378f99383035b0e937a539f2eb
Justin Garnica <Justin.garnica@suusuccess.org>
2018-11-09 08:36:52 -0700

    started program.

---
+++ Chapter 5/Ex_5_17.java
@@ -0,0 +1,14 @@
+/*
+Author: Justin Garnica
+Date: 11/9/2018
+
+Make a number pyramid
+*/
+import java.util.Scanner;
+
+public class Ex_5_17 {
+	public static void main(String[] args) {
+		
+		
+	}
+}
