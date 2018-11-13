24a94a01e3f3455e1be748945ac33593becfa9b5
Justin Garnica <Justin.garnica@suusuccess.org>
2018-11-13 08:07:18 -0700

    fixed program

--- Chapter 5/Ex_5_11.java
+++ Chapter 5/Ex_5_11.java
@@ -10,7 +10,7 @@ public class Ex_5_11 {
 		int count = 1;
 		for (int i = 100; i <= 200; i++) {
 			if (i % 6 == 0 ^ i % 5 == 0) {
-				System.out.print((count++ % 10 != 0) ? i + " " : i + "\n");
+				System.out.print((count++ % 10 != 0) ? + i + " " : + i + "\n");
 
 			}
 		}
