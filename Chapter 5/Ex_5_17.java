51cf39ea77f38b5341afed98b066fa58e1e30089
Justin Garnica <Justin.garnica@suusuccess.org>
2018-11-26 08:47:29 -0700

    finnished program

--- Chapter 5/Ex_5_17.java
+++ Chapter 5/Ex_5_17.java
@@ -8,6 +8,34 @@ import java.util.Scanner;
 
 public class Ex_5_17 {
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+			
+			System.out.print("Enter number of rows here (1 - 15): ");
+			int rows = input.nextInt();
+			
+			while (rows < 1 || rows > 15) {
+				System.out.print("Enter number of rows here (1 - 15): ");
+				rows = input.nextInt();
+
+			}
+			
+			for (int r = 1; r <= rows; r++) {
+
+				for (int spaces = 1; spaces <= (rows - r); spaces ++) {
+					System.out.print("   ");
+				}
+
+				for (int left = r; left >= 1; left--) {
+					System.out.printf("%3d", left);
+				}
+				
+				for (int right = 2; right <= r; right++) {
+					System.out.printf("%3d", right);
+				}
+				
+				System.out.println();
+			}
+		
 		
 		
 	}
