884aaa37bb8392d69c58c828771405083b56be08
Justin Garnica <justin.garnica@suusuccess.org>
2019-02-07 08:22:22 -0700

    Started

---
+++ Chapter 7/Ex_7_23.java
@@ -0,0 +1,16 @@
+/*
+Author: Justin Garnica
+Date: 2/7/2019
+
+Solve the locker puzzle.
+*/
+
+class Ex_7_23 {
+	public static void main(String[] args) {
+		for ( int i = 0; i < 100; i++){
+			
+		}
+		
+	}
+	
+}
