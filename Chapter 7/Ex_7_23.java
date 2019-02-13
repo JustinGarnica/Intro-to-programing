2f7aa778c11e106173594bbda545abda93bc8fe0
Justin Garnica <justin.garnica@suusuccess.org>
2019-02-13 09:56:30 -0700

    fixed

--- Chapter 7/Ex_7_23.java
+++ Chapter 7/Ex_7_23.java
@@ -7,20 +7,20 @@ Solve the locker puzzle.
 
 class Ex_7_23 {
 	public static void main(String[] args) {
-		Boolean[] locker = new Boolean[100];
+		boolean[] locker = new boolean[101];
 		int student = 1;
-		while (student <= 100) {
+		while (student < 101) {
 			for (int i = 0; i < locker.length; i++){
 				if (i % student == 0) {
 					locker[i] = !locker[i];
 				} 
-			student++;
 			}
+			student++;
 		}
 		System.out.print("The following lockers are open:");
-		for (int i = 0; i < 100; i++){
+		for (int i = 0; i < 101; i++){
 			if (locker[i] == true) {
-				System.out.print(" L" + locker[i]);
+				System.out.print(" L" + i);
 			}
 		}
 	}
