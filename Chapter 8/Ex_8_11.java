2a5a7234c23e1465ad2ae44cc4d9d5a1a3c5a984
Justin Garncia <justin.garnica@suusuccess.org>
2019-02-26 09:00:31 -0700

    almost got it.

--- Chapter 8/Ex_8_11.java
+++ Chapter 8/Ex_8_11.java
@@ -6,8 +6,41 @@ Heads or tails thingy
 */
 import java.util.Scanner;
 
-class Ex_8_11 {
+public class Ex_8_11{
 	public static void main(String[] args) {
+		java.util.Scanner input = new java.util.Scanner(System.in);
+		int[][] biNum = new int[3][3];
+
+		System.out.print("Enter a number between 0 and 511: ");
+		int decNum = input.nextInt();
+		int subNum = 256; 
+		int num = decNum;
+
+		for (int row = 0 ; row < 3; row++){
+			for (int column = 0 ; column < 3; column++) {
+				if(num >= subNum) {
+					biNum[row][column] = 1;
+					num = num - subNum;
+				}
+				else {
+					subNum = subNum / 2;
+					biNum[row][column] = 0;
+				}
+			}
+		}
 		
+		for (int row = 0; row < biNum.length; row++){
+			for (int column = 0; column < biNum[row].length; column++){
+				if (biNum[row][column] == 1){
+					System.out.print("T ");
+				}
+				else if (biNum[row][column] == 0){
+					System.out.print("H ");
+				}
+				if (column == 2){
+					System.out.print("\n");
+				}
+			}
+		}
 	}
 }
