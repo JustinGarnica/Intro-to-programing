37779fd980815bbf9ef8b20f7ce5c1845e13c167
Justin Garnica <justin.garnica@suusuccess.org>
2019-03-06 10:33:39 -0700

    alm0st

---
+++ Chapter 8/Ex_8_37.java
@@ -0,0 +1,25 @@
+/*
+Author: Justin Garnica
+Date: 3/4/2019
+
+state capitol thingy
+*/
+
+class Ex_8_37 {
+	public static void main(String[] args) {
+		String[][] sc = new String[][]{
+			{"florida", "tallahassee"},
+			{"georgia", "atlanta"},
+			{"wisconsin", "cheyenne"},
+			{"ohio", "columbus"},
+			{"illinois", "springfeild"},
+			{"texas", "austin"},
+			{"alabama", "montgomery"},
+			{"california", "sacramanto"},
+			{"maine", "augusta"},
+			{"mississississippi", "jackson"},
+		}; 
+		int specialnum = math.round(math.random * 9);
+		
+	}
+}
